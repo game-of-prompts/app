bd177093b0904684305fa3ac182cbf141287f393
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-12-26 12:28:15 +0100

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,26 @@ import path from "path";
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		globals: true, 
-		environment: 'node', 
+		globals: true,
+		environment: 'node',
 	},
-  	resolve: {
-    		alias: {
-      			$lib: path.resolve("./src/lib"),
-    		},
-  	}
+	resolve: {
+		alias: {
+			$lib: path.resolve("./src/lib"),
+		},
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			loader: {
+				'.es': 'text',
+			},
+		},
+	},
 });
 
 const config = {
 	// …
 	ssr: {
-	  noExternal: ['three']
+		noExternal: ['three']
 	}
-  }
+}
